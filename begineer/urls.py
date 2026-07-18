51e9b8d271780ba44481792a6073da171afec3e4
Sudin7 <sudinsharma14@gmail.com>
2026-07-18 15:25:31 +0545

    Update urls.py

--- begineer/urls.py
+++ begineer/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
